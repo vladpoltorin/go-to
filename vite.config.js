b9b0ac446d14469869f2dc4b7d13c8a09cbccbb7
Vlad Poltorin <vlad@getoctane.io>
2024-04-05 19:56:12 -0400

    fix: update vite config to load separate bundles for bg, popup and other pages. add tailwind

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   build: {
     rollupOptions: {
       input: {
-        popup: './src/popup/popup.tsx',
-        options: './src/options/options.tsx',
+        popup: './popup.html',
+        options: './options.html',
         background: './src/background.ts',
       },
       output: {
-        dir: 'dist',
+        dir: 'dist/',
         entryFileNames: '[name].js', // Specifies that the output files should be named after their entry names without hashes
         // format: 'esm', // or 'esm' or 'cjs' depending on your target environment
         // sourcemap: true,
