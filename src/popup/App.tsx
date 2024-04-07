aa145f3ea74b641faba17b20834cf78c756f4675
Vlad Poltorin <vlad@getoctane.io>
2024-04-07 11:51:25 -0400

    fix: update popup css

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,24 +1,27 @@
 import { useState } from "react";
+import './popup.css';
 
 export const App = () => {
   const [input, setInput] = useState('');
 
-  const onClick = async () => {
+  const handleCreate = async () => {
+    if (!input) return;
+
     const [currentTab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true  });
     await chrome.storage.local.set({ [input]: currentTab.url });
     setInput('');
   };
 
   return (
-    <div style={{ width: 300, height: 300 }}>
+    <div className="popup">
       <h3>Create a link for this page</h3>
       <label htmlFor="input">
-        <p><code>to/{input}</code></p>
+        <p><code className="codespan">to/{input}</code></p>
       </label>
       <input id="input" value={input} onChange={(e) => setInput(e.target.value)} />
-      <button onClick={onClick}>Create</button>
+      <button className="link_btn" disabled={!input} onClick={handleCreate}>Create</button>
 
-      <a href="../../dist/options.html" target="_blank">Manage links</a>
+      <a className="manage-link" href="../../dist/options.html" target="_blank">Manage links</a>
     </div>
   );
 };
