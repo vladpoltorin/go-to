3b09c5656214a4e6045d374cd3fbbcec5154f870
Vlad Poltorin <vlad@getoctane.io>
2024-04-09 16:19:29 -0400

    fix: fix shortcuts handling for chrome

--- src/background.ts
+++ src/background.ts
@@ -17,14 +17,38 @@ chrome.webNavigation.onErrorOccurred.addListener((details) => {
 });
 
 chrome.webNavigation.onBeforeNavigate.addListener((details) => {
+  if (details.frameType !== 'outermost_frame') {
+    return;
+  }
+
   const slug = details.url.split('/to/')[1];
 
   if (aliases[slug]) {
     const urlToGo = aliases[slug];
-
     chrome.tabs.update(details.tabId, { url: urlToGo });
   }
-}, { url: [{ urlMatches: '' }] });
+}, { url: [{ urlMatches: 'http://to/*' }] });
+
+chrome.webNavigation.onBeforeNavigate.addListener((details) => {
+  if (details.frameType !== 'outermost_frame') {
+    return;
+  }
+
+  const url = new URL(details.url);
+  const search = new URLSearchParams(url.search);
+  const query = search.get('q');
+
+  if (!query || !query.includes('to/')) {
+    return;
+  }
+
+  const slug = query.split('to/')[1];
+
+  if (aliases[slug]) {
+    const urlToGo = aliases[slug];
+    chrome.tabs.update(details.tabId, { url: urlToGo });
+  }
+}, { url: [{ urlMatches: 'https://www.google.com/search/*' }] });
 
 chrome.runtime.onInstalled.addListener(({reason}) => {
   if (reason === 'install') {
